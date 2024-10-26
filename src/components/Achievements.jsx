fef2a445282f4ceb45874f6482520d9f99a06d75
tazmanianDeviloper <tdavoodi@bu.edu>
2024-10-26 15:11:56 -0400

    fixed PageLayout import again

--- src/components/Achievements.jsx
+++ src/components/Achievements.jsx
@@ -3,9 +3,9 @@ import dl from '../images/dl.jfif'
 export default function Achievements() {
     return (
         <PageLayout headerinfo="Achievements" paragraph="Awards received">
-            <h2>Dean's List</h2>
+            <h2>Dean&#39;s List</h2>
             <img src={dl} width="20%" alt="Dean's List" />
-            <p>I was awarded the dean's list for multiple semesters at BU.</p>
+            <p>I was awarded the dean&#39;s list for multiple semesters at BU.</p>
         </PageLayout>
     )
 }
