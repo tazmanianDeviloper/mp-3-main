c05f3f080b8d21d0fc03e4cc6973d3f20052990b
tazmanianDeviloper <tdavoodi@bu.edu>
2024-10-26 15:29:50 -0400

    fixed PageLayout import 5 places

--- src/components/References.jsx
+++ src/components/References.jsx
@@ -1,5 +1,5 @@
 import { styled } from 'styled-components'
-import PageLayout from './PageLayOut.jsx'
+import PageLayout from './PageLayout.jsx'
 
 export default function References() {
     return (
