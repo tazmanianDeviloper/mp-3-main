cca9a0b3e9fbdf0aef4b0ed15b46496248d061d4
tazmanianDeviloper <tdavoodi@bu.edu>
2024-10-26 15:24:32 -0400

    fixed PageLayout import 4 places

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import { styled } from 'styled-components';
-import PageLayout from './PageLayOut';
+import PageLayout from './PageLayOut.jsx';
 import Calculator from './Calculator';
 
 export default function Projects() {
